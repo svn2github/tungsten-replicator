d678187aa3d9eed9c512b4a8cd7689b48da093e2
ludovic.launer@continuent.com <ludovic.launer@continuent.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2013-06-25 07:49:33 +0000

    TUC-1703 Add method to create from non case sensitive String
    git-svn-id: http://tungsten-replicator.googlecode.com/svn/trunk@1451 9f5e3340-5c9a-25cb-7c1c-c133a2e9e928

--- commons/src/java/com/continuent/tungsten/common/cluster/resource/ResourceState.java
+++ commons/src/java/com/continuent/tungsten/common/cluster/resource/ResourceState.java
@@ -1,8 +1,29 @@
 
 package com.continuent.tungsten.common.cluster.resource;
 
+import java.text.MessageFormat;
+
+import com.continuent.tungsten.common.exception.ResourceException;
+
 public enum ResourceState
 {
-    ONLINE, OFFLINE, SYNCHRONIZING, JOINING, SUSPECT, STOPPED, UNKNOWN, TIMEOUT, DEGRADED, SHUNNED, CONSISTENT, MODIFIED, FAILED, BACKUP, UNREACHABLE, EXTENSION, RESTORING,
+    ONLINE, OFFLINE, SYNCHRONIZING, JOINING, SUSPECT, STOPPED, UNKNOWN, TIMEOUT, DEGRADED, SHUNNED, CONSISTENT, MODIFIED, FAILED, BACKUP, UNREACHABLE, EXTENSION, RESTORING;
+
+    /**
+     * Create a ResourceState from a non case sensitive String
+     * 
+     * @param x the string to be converted
+     * @return the converted ResourceState
+     * @throws ResourceException if could not cast string to a ResourceState
+     */
+    public static ResourceState fromString(String x) throws ResourceException
+    {
+        for (ResourceState currentType : ResourceState.values())
+        {
+            if (x.equalsIgnoreCase(currentType.toString()))
+                return currentType;
+        }
+        throw new ResourceException(MessageFormat.format("Cannot cast into a known ResourceState: {0}", x));
+    }
 
 }
