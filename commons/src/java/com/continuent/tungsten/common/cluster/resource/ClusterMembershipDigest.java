b4dafbf07a08e28e15696a6b5f0865f289192839
edward.archibald@continuent.com <edward.archibald@continuent.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2013-06-26 18:11:21 +0000

    TUC-1722 Added missing output that shows GC group membership.
    git-svn-id: http://tungsten-replicator.googlecode.com/svn/trunk@1457 9f5e3340-5c9a-25cb-7c1c-c133a2e9e928

--- commons/src/java/com/continuent/tungsten/common/cluster/resource/ClusterMembershipDigest.java
+++ commons/src/java/com/continuent/tungsten/common/cluster/resource/ClusterMembershipDigest.java
@@ -176,7 +176,7 @@ public class ClusterMembershipDigest implements Serializable
             if (verbose)
             {
                 CLUtils.println("MEMBERSHIP IS CONSISTENT");
-                CLUtils.println("UNVALIDATED CURRENT MEMBERS ARE:\n"
+                CLUtils.println("GC VIEW OF CURRENT MEMBERS ARE:\n"
                         + CLUtils.iterableToString(getUnvalidatedMembers()));
                 CLUtils.println("VALIDATED CURRENT MEMBERS ARE:\n"
                         + CLUtils.iterableToString(getValidMembers()));
@@ -187,6 +187,8 @@ public class ClusterMembershipDigest implements Serializable
         if (verbose)
         {
             CLUtils.println("MEMBERSHIP IS NOT CONSISTENT");
+            CLUtils.println("GC VIEW OF CURRENT MEMBERS ARE:\n"
+                    + CLUtils.iterableToString(getUnvalidatedMembers()));
             CLUtils.println("VALIDATED CURRENT MEMBERS ARE:\n"
                     + CLUtils.iterableToString(getValidMembers()));
             CLUtils.println("UNREACHABLE MEMBERS ARE:\n"
