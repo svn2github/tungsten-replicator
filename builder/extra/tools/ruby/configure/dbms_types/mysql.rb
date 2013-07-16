bc546dd031b9d4d5bc7db76cea379b7d730d2447
g.maxia@gmail.com <g.maxia@gmail.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2013-07-16 12:44:46 +0000

    Removed the MySQLCheckSumCheck validation check.
    git-svn-id: http://tungsten-replicator.googlecode.com/svn/trunk@1509 9f5e3340-5c9a-25cb-7c1c-c133a2e9e928

--- builder/extra/tools/ruby/configure/dbms_types/mysql.rb
+++ builder/extra/tools/ruby/configure/dbms_types/mysql.rb
@@ -1003,20 +1003,24 @@ module ConfigureDeploymentStepMySQL
   end
 end
 
-class MySQLCheckSumCheck < ConfigureValidationCheck
-  include ReplicationServiceValidationCheck
-  include MySQLApplierCheck
-  include NotPrefetchCheck
-  
-  def set_vars
-    @title = "MySQL 5.6 binlog Checksum Check"
-  end
-  
-  def validate
-    info("Checking that MySQL Binlog Checksum is not enabled")
-    checkSum = get_applier_datasource.get_value("show variables like 'binlog_checksum'", "Value")
-    if (checkSum == 'CRC32') 
-      error("This instance is running with BinLog checksum enabled which is not yet supported")
-    end
-  end
-end
+#
+# Removed check that prevents installation using MySQL 5.6 servers
+# Starting with Tungsten-Replicator 2.1.1-101, MySQL 5.6 is fully supported
+#
+#class MySQLCheckSumCheck < ConfigureValidationCheck
+#  include ReplicationServiceValidationCheck
+#  include MySQLApplierCheck
+#  include NotPrefetchCheck
+#  
+#  def set_vars
+#    @title = "MySQL 5.6 binlog Checksum Check"
+#  end
+#  
+#  def validate
+#    info("Checking that MySQL Binlog Checksum is not enabled")
+#    checkSum = get_applier_datasource.get_value("show variables like 'binlog_checksum'", "Value")
+#    if (checkSum == 'CRC32') 
+#      error("This instance is running with BinLog checksum enabled which is not yet supported")
+#    end
+#  end
+#end
