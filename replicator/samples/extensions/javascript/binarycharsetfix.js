d7cbddde6817ce537ab65705590794c6686b1e8d
robert.hodges@continuent.com <robert.hodges@continuent.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2013-11-16 18:58:53 +0000

    Issue 743 Implemented supplementary Javascript file illustrating how to fix up character sets.
    git-svn-id: http://tungsten-replicator.googlecode.com/svn/trunk@1821 9f5e3340-5c9a-25cb-7c1c-c133a2e9e928

---
+++ replicator/samples/extensions/javascript/binarycharsetfix.js
@@ -0,0 +1,94 @@
+/**
+ * Tungsten Scale-Out Stack
+ * Copyright (C) 2007-2013 Continuent Inc.
+ * Contact: tungsten@continuent.org
+ *
+ * Java script example showing how to mutate an invalid character set and 
+ * and collation on a statement into one that can be processed on the slave. 
+ *
+ * Instructions for deployment.  
+ *
+ * 1.) Copy this file to directory tungsten-replicator/samples/extensions/javascript
+ *
+ * 2.) Locate the properties file for your replication service.  It has a name 
+ * like tungsten-replicator/conf/static-mysvc.properties.  
+ * 
+ * 3.) Edit the FILTERS section of the properties file and add the following
+ * lines.  Edit the server IDs you want to omit to use your own values. 
+
+ * replicator.filter.binarycharsetfix=com.continuent.tungsten.replicator.filter.JavaScriptFilter
+ * replicator.filter.binarycharsetfix.script=${replicator.home.dir}/samples/extensions/javascript/binarycharsetfix.js
+ * 
+ * 4.) Edit the pipelines section of the properties file and change the 
+ * following line from: 
+ * 
+ * replicator.stage.binary-to-q.filters=
+ * 
+ * to: 
+ *
+ * replicator.stage.binary-to-q.filters=binarycharsetfix
+ *
+ * 5.) Save the file and restart the replicator.  
+ * 
+ * @author <a href="mailto:linas.virbalas@continuent.com">Linas Virbalas</a>
+ * @author <a href="mailto:robert.hodges@continuent.com">Robert Hodges</a>
+ */
+
+/**
+ * Called once when JavaScriptFilter corresponding to this script is prepared.
+ */
+function prepare()
+{
+    logger.info("binarycharsetfix initialized");    
+}
+
+/**
+ * Filter charset in current event and change 63 (binary) to 47 (Latin1 binary)
+ * Do this for statements only.  We alter the stated options for charset. 
+ */
+function filter(event)
+{
+    // Find the DBMS data. 
+    data = event.getData();
+
+    // One ReplDBMSEvent may contain many DBMSData events.
+    for(i = 0; i < data.size(); i++)
+    {
+          // Get com.continuent.tungsten.replicator.dbms.DBMSData
+          d = data.get(i);
+
+          // Look for statement data. 
+          if(d instanceof com.continuent.tungsten.replicator.dbms.StatementData)
+          {   
+              // Now look for charset equal to 63. 
+              charset_id = d.getOption("character_set_client");
+              if (charset_id == 63)
+              {
+                  // If this does not happen too often a comment is nice. 
+                  logger.info("Patching up charset: seqno=" + event.getSeqno() 
+                     + " fragno=" + event.getFragno() + " statement number=" + i);
+
+                  // Drop old values. Options are stored as name value pairs, 
+                  // so we have to cycle through the option list backwards
+                  // to remove them. 
+                  options = d.getOptions();
+                  for (o = options.size() - 1; o >= 0; o--)
+                  {
+                      option = options.get(o);
+                      name = option.getOptionName();
+                      if (name == "character_set_client" || name == "collation_connection")
+                      {
+                          if (options.remove(option))
+                          {
+                              logger.debug("Removed option: " + name);
+                          }
+                      }
+                  }
+
+                  // Add replacement values.  
+                  d.addOption("character_set_client", 47);
+                  d.addOption("collation_connection", 47);
+              }
+          }
+    }
+}
